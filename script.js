cc54f4a17d1edcaeef20124c1aa6f9da9a786120
Frankie Adams <fadams@EMCMIDDLEBURG.LOCAL>
2022-05-27 13:06:33 -0400

    started creating challenge

---
+++ script.js
@@ -0,0 +1,16 @@
+const chart = document.getElementById('chart');
+const days = document.getElementById('days');
+
+fetch('data.json')
+.then(response => response.json())
+.then(data => {
+ data.forEach(function (item) {
+   console.log(item.amount)
+
+   const h5 = document.createElement('h5');
+   const content = document.createTextNode(item.day);
+   h5.appendChild(content)
+   chart.appendChild(h5)
+   
+ })
+})
